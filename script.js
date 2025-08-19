781afef507b3b8486cb25d88f54e091e049e05a4
lottiejudge <lottielucyjudge@gmail.com>
2025-08-19 13:00:18 +0100

    functionality and styling for the overlay

--- script.js
+++ script.js
@@ -8,4 +8,18 @@ function showSlide() {
 }
 
 setInterval(showSlide, 3000);  
-  
+
+// adding in functionality for the logo overlay 
+document.addEventListener('DOMContentLoaded', function() {
+  const overlay = document.getElementById('logo-overlay');
+  function showMainSite() {
+      document.body.classList.add('show-main');
+      overlay.style.display = 'none';
+      // Optionally, remove event listeners here
+  }
+  overlay.addEventListener('click', showMainSite);
+  overlay.addEventListener('keydown', showMainSite);
+  overlay.addEventListener('mousemove', showMainSite);
+  overlay.tabIndex = 0; // Make it focusable for keydown
+  overlay.focus();
+});
